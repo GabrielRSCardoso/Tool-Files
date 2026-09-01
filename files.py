a745132f2e74ca3d1663a0bab7bc854c0d88de30
Gabriel <gabrielreiscardoso10@gmail.com>
2026-09-01 18:48:31 -0300

    funcao pasta_destino

--- files.py
+++ files.py
@@ -3,19 +3,21 @@ import shutil
 
 origem = Path.home() / "Documentos" / "pasta_teste"
 
+def pasta_destino(item) -> Path:
+    tipo = item.suffix
+    if not tipo:
+        tipo = "sem_extensao"
+    tipo_nome = tipo.upper().removeprefix(".")
+    destino = Path.home() / "Documentos" / "pasta_teste" / tipo_nome
+    destino.mkdir(exist_ok=True)
+    return destino
+
 for item in origem.iterdir():    
     if item.is_file():
-        tipo = item.suffix
-        if not tipo:
-            tipo = "sem_extensao"
-        tipo_nome = tipo.upper()
-        tipo_nome = tipo_nome.removeprefix(".")
-        destino = Path.home() / "Documentos" / "pasta_teste" / tipo_nome
-        destino.mkdir(exist_ok=True)
+        destino = pasta_destino(item)
         try:
             shutil.move(item, str(destino))
             print(f"{item.name} -> {destino.name}")
         except (PermissionError, shutil.Error, OSError) as e:
             print(f"Erro ao mover {item.name}: {e}")
-        
-        
+
