ee83d035a5117ac151297ef13bd801b7fd1d35ba
Gabriel <gabrielreiscardoso10@gmail.com>
2026-09-02 19:42:12 -0300

    update logs

--- files.py
+++ files.py
@@ -1,7 +1,19 @@
 from pathlib import Path
 import shutil
+import logging
+
+logging.basicConfig(
+    filename="logs.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 origem = Path.home() / "Documentos" / "pasta_teste"
+
+
+if not origem.exists():
+    logging.critical(f"Pasta não encontrada: {origem}")
+    exit()
 
 def pasta_destino(item) -> Path:
     tipo = item.suffix
@@ -12,12 +24,13 @@ def pasta_destino(item) -> Path:
     destino.mkdir(exist_ok=True)
     return destino
 
+
 for item in origem.iterdir():    
     if item.is_file():
         destino = pasta_destino(item)
         try:
             shutil.move(item, str(destino))
-            print(f"{item.name} -> {destino.name}")
+            logging.info(f"{item.name} -> {destino.name}")
         except (PermissionError, shutil.Error, OSError) as e:
-            print(f"Erro ao mover {item.name}: {e}")
+            logging.error(f"Erro ao mover {item.name}: {e}")
 
